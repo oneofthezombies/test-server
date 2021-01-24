fb4dc565afc32b287b7cb5750cfd8bd8bc88fa39
hunhoekim <hunhoekim@gmail.com>
2021-01-24 15:35:35 +0900

    modified Precompile.h code style

---
+++ Precompile.h
@@ -0,0 +1,20 @@
+//
+// Created by hunhoekim on 2021/01/24.
+//
+
+#ifndef Precompile_h
+#define Precompile_h
+
+#include <array>
+#include <cassert>
+#include <cstdint>
+#include <iostream>
+#include <optional>
+#include <sstream>
+#include <string>
+#include <type_traits>
+#include <unordered_map>
+#include <variant>
+#include <vector>
+
+#endif // Precompile_h
